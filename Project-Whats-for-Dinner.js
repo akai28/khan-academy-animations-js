65e40cbb2c96659dd7b35205af6b4baa54c1472a
akai28 <45823160+akai28@users.noreply.github.com>
2019-04-29 13:15:56 +0000

    Create Project-Whats-for-Dinner.js

---
+++ Project-Whats-for-Dinner.js
@@ -0,0 +1,97 @@
+// wooden table
+background(186, 145, 20); 
+
+// plate
+fill(0, 255, 225);
+ellipse(200, 200, 350, 350);
+
+fill(0, 111, 255);
+ellipse(200, 200, 300, 300);
+
+//chapati
+fill(235, 235, 150);
+ellipse(209,100,90,90);
+ellipse(237,121,90,90);
+strokeWeight(5);
+point(217, 106);
+point(260,137);
+point(207, 73);
+
+//bhaji
+strokeWeight(1);
+fill(255, 136, 0);
+ellipse(116,145,90,90);
+image(getImage("cute/TreeShort"), 110, 121, 10, 20);
+image(getImage("cute/TreeShort"), 100, 135, 10, 20);
+image(getImage("cute/TreeShort"), 120, 136, 10, 20);
+
+//rasgulla
+fill(252, 247, 247);
+ellipse(296,179,30,30);
+ellipse(321,179,30,30);
+
+//bowl
+fill(230, 113, 154);
+arc(304, 179, 70, 60, 1,180 );
+
+//salad
+fill(240, 63, 19);
+ellipse(195,191,20,20);
+ellipse(222,205,20,20);
+ellipse(213,212,20,20);
+
+fill(168, 9, 96);
+ellipse(213,191,20,20);
+ellipse(195,219,20,20);
+
+fill(131, 199, 162);
+ellipse(205,202,20,20);
+ellipse(186,205,20,20);
+
+image(getImage("avatars/leaf-green"), 197,170,20,20);
+image(getImage("avatars/leaf-green"), 210,204,20,20);
+image(getImage("avatars/leaf-yellow"), 176,181,20,20);
+
+//soup
+fill(230, 85, 85);
+ellipse(254,279,100,90);
+image(getImage("cute/TreeShort"), 250, 266, 10, 20);
+image(getImage("cute/TreeShort"), 241, 274, 10, 20);
+
+//crutons
+fill(92, 26, 54);
+rect(240,272,5,3);
+rect(267,283,5,3);
+rect(257,289,5,3);
+rect(265,265,5,3);
+
+//spoon
+fill(159, 171, 171);
+rotate(11);
+rect(113,230,8,90);
+ellipse(117,206,30,50);
+
+//chhas
+fill(235, 231, 197);
+rotate(-10);
+translate(-109,-30);
+quad(214, 310, 315, 311, 287, 350, 241, 349);
+
+stroke(252, 102, 15);
+strokeWeight(10);
+point(267, 263);
+point(273, 269);
+point(259, 270);
+point(275, 263);
+point(258, 263);
+point(267, 276);
+
+
+//https://www.khanacademy.org/computer-programming/spin-off-of-project-whats-for-dinner/5256661207449600
+
+
+
+
+
+
+
